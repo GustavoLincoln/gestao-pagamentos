d828326cb4121481b7a23e6c67c657168a624bbb
GustavoLincoln <41446763+GustavoLincoln@users.noreply.github.com>
2024-05-17 19:35:48 -0300

    Adicionado o Bootstrap

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
